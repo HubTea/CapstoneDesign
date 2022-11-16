457a02ec8cd4b3b9678d9e1f26e8194e74510f54
HubTea <k3392255@naver.com>
2022-11-17 00:12:00 +0900

    Configuration schema 정의

---
+++ Server/src/configSchema.ts
@@ -0,0 +1,49 @@
+import joi from 'joi';
+import { makeSchema } from './utility/joi';
+
+export class DatabaseConfig {
+    host: string = '';
+    port: number = 0;
+    database: string = '';
+    username: string = '';
+    password: string = '';
+}
+
+export let databaseConfigSchema = makeSchema<DatabaseConfig>({
+    host: joi.string().required(),
+    port: joi.number().required(),
+    database: joi.string().required(),
+    username: joi.string().required(),
+    password: joi.string().required()
+});
+
+export class DatabaseReplicationConfig {
+    write: DatabaseConfig = new DatabaseConfig();
+    read: DatabaseConfig[] = [];
+}
+
+export let databaseReplicationConfigSchema = 
+makeSchema<DatabaseReplicationConfig>({
+    write: databaseConfigSchema,
+    read: joi.array().items(databaseConfigSchema).required()
+});
+
+export class Config {
+    databaseReplication: DatabaseReplicationConfig = 
+        new DatabaseReplicationConfig();
+}
+
+export let configSchema = makeSchema<Config>({
+    databaseReplication: databaseReplicationConfigSchema
+});
+
+// export class StaticConfig {
+//     configServerHost: string = '';
+//     configServerPort: number = 0;
+// }
+
+// let staticConfigSchemaDefinition: SamePropertyName<StaticConfig> = {
+//     configServerHost: joi.string().required(),
+//     configServerPort: joi.number().required()
+// }
+// export let staticConfigSchema = joi.object(staticConfigSchemaDefinition);
