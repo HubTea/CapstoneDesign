43e499372c7c5137ba2b7f5f86619e9263fe5cd6
HubTea <k3392255@naver.com>
2022-11-19 17:16:21 +0900

    DatabaseConnectionContainer의 동작을 수정
    Database에 접속하기 전에 들어온 요청을 대기시키기보단 에러를 응답하는 것이
    자연스럽다.
    애플리케이션 실행 중에 사용할 sequelize 객체가 바뀌므로 repository mode를
    사용해야 한다.
    
    RepositoryCollection, DatabaseConnection 클래스를 만들었다.
    DatabaseConnectionContainer.get()이 DatabaseConnection을 반환한다.

--- Server/src/service/databaseConnectionContainer.ts
+++ Server/src/service/databaseConnectionContainer.ts
@@ -1,37 +1,59 @@
 import { Injectable } from '@nestjs/common';
-import { Sequelize } from 'sequelize-typescript';
+import {
+    Sequelize, 
+    Repository 
+} from 'sequelize-typescript';
 import { Config } from '../configSchema';
+import * as model from '../model';
+
+export class RepositoryCollection {
+    user: Repository<model.User>;
+    post: Repository<model.Post>;
+    comment: Repository<model.Comment>;
+    history: Repository<model.History>;
+    fish: Repository<model.Fish>;
+    category: Repository<model.Category>;
+
+    constructor(sequelize: Sequelize) {
+        this.user = sequelize.getRepository(model.User);
+        this.post = sequelize.getRepository(model.Post);
+        this.comment = sequelize.getRepository(model.Comment);
+        this.history = sequelize.getRepository(model.History);
+        this.fish = sequelize.getRepository(model.Fish);
+        this.category = sequelize.getRepository(model.Category);
+    }
+}
+
+export class DatabaseConnection {
+    constructor(
+        public sequelize: Sequelize, 
+        public repository: RepositoryCollection
+    ) {
+
+    }
+}
 
 @Injectable()
 export class DatabaseConnectionContainer {
-    sequelize?: Sequelize;
-    resolve?: any;
-    reject?: any;
+    private connection?: DatabaseConnection;
 
     update(config: Config) {
-        let oldSequelize = this.sequelize;
-
-        this.sequelize = new Sequelize({
+        let sequelize = new Sequelize({
             dialect: 'postgres',
-            replication: config.databaseReplication
-        }) 
+            replication: config.databaseReplication,
+            repositoryMode: true
+        });
+        let repository = new RepositoryCollection(sequelize);
 
-        if(!oldSequelize && this.resolve) {
-            this.resolve(this.sequelize);
-        }
+        this.connection = new DatabaseConnection(sequelize, repository);
     }
 
-    async get(): Promise<Sequelize> {
-        if(this.sequelize) {
-            return this.sequelize;
+    get(): DatabaseConnection {
+        if(this.connection) {
+            return this.connection;
         }
         else {
-            return new Promise(this.execute.bind(this));
-        } 
-    }
-
-    execute(resolve: any, reject: any) {
-        this.resolve = resolve;
-        this.reject = reject;
+            throw new Error('connection not initialized');
+        }
     }
 }
