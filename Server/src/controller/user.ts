214c02b7e99efc306bf24e277827b0339916a247
HubTea <k3392255@naver.com>
2022-11-23 22:42:46 +0900

    유저 등록 Controller, Service 작성
    UserController, UserService 클래스를 만들었다.

---
+++ Server/src/controller/user.ts
@@ -0,0 +1,29 @@
+import { 
+    Controller,
+    Post,
+    Body
+} from '@nestjs/common';
+import { 
+    DatabaseConnectionContainer 
+} from '../service/databaseConnectionContainer';
+import { UserService } from '../service/user';
+import {
+    UserCreationDto
+} from '../dto';
+
+@Controller('/user')
+export class UserController {
+    constructor(
+        readonly databaseConnectionContainer: DatabaseConnectionContainer,
+        readonly userService: UserService
+    ) {
+
+    }
+
+    @Post()
+    async register(@Body() userCreationDto: UserCreationDto) {
+        let connection = this.databaseConnectionContainer.get();
+
+        await this.userService.register(connection.repository, userCreationDto);
+    }
+}
---
+++ Server/src/service/user.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { UniqueConstraintError } from 'sequelize';
+import { RepositoryCollection } from './databaseConnectionContainer';
+import { UserCreationDto } from '../dto';
+import { AlreadyExist } from '../error';
+
+@Injectable()
+export class UserService {
+    async register(
+        repository: RepositoryCollection, 
+        userCreationDto: UserCreationDto
+    ) {
+        let salt = await bcrypt.genSalt();
+        let hash = await bcrypt.hash(userCreationDto.password, salt);
+
+        try{
+            await repository.user.create({
+                nickname: userCreationDto.nickname,
+                account: userCreationDto.account,
+                salt: salt,
+                hash: hash,
+                hashVersion: '0',
+                admin: false
+            })
+        }
+        catch(err){
+            if(err instanceof UniqueConstraintError) {
+                throw new AlreadyExist(err);
+            }
+            else {
+                throw err;
+            }
+        }
+        
+    }
+}
+
