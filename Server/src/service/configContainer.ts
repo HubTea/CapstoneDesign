aced71ff8d38e61db953ab9d0cd818f2d746a3af
HubTea <k3392255@naver.com>
2022-11-23 22:25:58 +0900

    ConfigContainer 클래스 작성

---
+++ Server/src/service/configContainer.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { Config } from '../configSchema';
+
+@Injectable()
+export class ConfigContainer {
+    config?: Config;
+
+    update(config: Config) {
+        this.config = config;
+    }
+
+    get(): Config {
+        if(this.config) {
+            return this.config;
+        }
+        else {
+            throw new Error('config not initialized');
+        }
+    }
+}
