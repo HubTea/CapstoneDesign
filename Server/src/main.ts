8990ea09ef0c5ba702b878fc5471c72bd0680aeb
HubTea <k3392255@naver.com>
2022-11-29 01:10:14 +0900

    포트 번호 변경

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule }from './module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(443);
+  await app.listen(80);
   console.log('listen');
 }
 
