7c359155907685642fe9f983c97a10d195d6eda5
HubTea <k3392255@naver.com>
2023-10-17 13:45:34 +0900

    외부에서 받는 설정 객체 유효성 검증
    네트워크를 통해 외부에서 설정 객체를 받는 경우 유효성 검증 로직이 없어 추가했다.

--- Server/src/controller/agent.ts
+++ Server/src/controller/agent.ts
@@ -1,11 +1,15 @@
 import { Controller, Body, Post } from '@nestjs/common';
-import { Config } from '../configSchema';
+import { Config, configSchema } from '../configSchema';
 import { bridge } from '../service/bridge';
 
 @Controller('/config')
 export class AgentController {
   @Post()
   update(@Body() config: Config) {
+    const result = configSchema.validate(config);
+    if (result.error) {
+      throw new Error(`${result}`);
+    }
     bridge.emit(config);
   }
 }
